31bbff69d0989ccdd8ba111e5e96cf8c3a7243c9
monnttt <118706500+monnttt@users.noreply.github.com>
2024-01-10 20:37:33 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Media Online</h1>
+    <p><b>Sosial Media Developer</b></p>
+    <p>Belajar dan berbagai agar hidup menjadi lebih baik</p>
+    <p><b>Benfit Join di Media Online</b></p>
+    <ul><li>Mendapatkan motivasi dari sesama para Developer</li></ul>
+    <ul><li>Sharing knowlenge</li></ul>
+    <ul><li>Dibuat oleh calon web Developer terbaik</li></ul>
+    <p><b>Cara Bergabung ke Media Online</b></p>
+    <ol>
+        <li>Mengunjungi Website ini</li>
+        <li>Mendaftarkan di <a href="form.php">Form Sign up</a></li>
+        <li>Selesai</li>
+    </ol>
+   
+ 
+
+
+
+</body>
+</html>
